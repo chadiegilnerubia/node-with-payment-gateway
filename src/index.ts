77c070c88175feebb27cad0a3f95d6ecb0e19c40
Chadie Gil <caugis@nerubia.com>
2024-05-11 18:35:03 +0800

    initial

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Xendit } from "xendit-node";
+import { CreateInvoiceRequest, Invoice } from "xendit-node/invoice/models";
+
+// Load environment variables (.env file required)
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+const invoiceFunc = async () => {
+  console.log("invoice function called");
+  const secretKey = process.env.SECRET_KEY;
+
+  if (secretKey === undefined) {
+    console.error("Error: Missing environment variable SECRET_KEY");
+    return undefined; // Return undefined if secretKey is missing
+  }
+
+  const xenditClient = new Xendit({
+    secretKey,
+  });
+  const { Invoice } = xenditClient;
+  const data: CreateInvoiceRequest = {
+    externalId: "test123",
+    amount: 23232,
+    currency: "PHP",
+    customer: {
+      givenNames: "Ahmad",
+      surname: "Gunawan",
+      email: "ahmad_gunawan@example.com",
+      mobileNumber: "+6287774441111",
+    },
+    customerNotificationPreference: {
+      invoicePaid: ["email", "whatsapp"],
+    },
+    successRedirectUrl: "example.com/success",
+    failureRedirectUrl: "example.com/failure",
+    items: [
+      {
+        name: "Double Cheeseburger",
+        quantity: 1,
+        price: 7000,
+        category: "Fast Food",
+      },
+      {
+        name: "Chocolate Sundae",
+        quantity: 1,
+        price: 3000,
+        category: "Fast Food",
+      },
+    ],
+    fees: [
+      {
+        type: "Delivery",
+        value: 10000,
+      },
+    ],
+  };
+  const response: Invoice = await Invoice.createInvoice({ data });
+  console.log("response", response.invoiceUrl);
+};
+invoiceFunc();
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
